d26fa7cba9cee92c73d94e18ca57987be69430ac
Miguel <miguel.amaral.sales@gmail.com>
2024-09-09 22:36:38 -0300

    go

---
+++ User-Service/core/src/main/java/br/com/core/domain/entities/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package br.com.core.domain.entities.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
